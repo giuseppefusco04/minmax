091dc91584948f9fa4aab4e48cc811abbe26ea06
Giuseppe Fusco <GIUSEPPE.FUSCO@local.ittcesena.it>
2021-10-01 09:31:43 +0200

    v1.1: aggiunta calcolo minimo

--- minmax/minmax/Program.cs
+++ minmax/minmax/Program.cs
@@ -6,7 +6,18 @@ namespace minmax
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] vet = new int[5] { 3, 6, -3, 5, 7 };
+
+            int min = vet[0];
+            for(int i = 1; i < vet.Length; i++)
+            {
+                if (vet[i] < min)
+                {
+                    min = vet[i];
+                }
+            }
+            Console.WriteLine("MINIMO: "+min);
+            Console.ReadKey();
         }
     }
 }
